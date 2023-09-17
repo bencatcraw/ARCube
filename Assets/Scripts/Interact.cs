f9f5495f458c9a0cb8731b00b49150ac871f0725
bencatcraw <bencatcraw@gmail.com>
2023-09-16 18:00:24 -1000

    ye

--- Assets/Scripts/Interact.cs
+++ Assets/Scripts/Interact.cs
@@ -8,11 +8,14 @@ public class Interact : MonoBehaviour
     [SerializeField] private Transform dLight;
     public float rotationSpeed = 20f;
 
+    private bool isFingerDown = false;
+
     private void OnEnable()
     {
         EnhancedTouch.TouchSimulation.Enable();
         EnhancedTouch.EnhancedTouchSupport.Enable();
         EnhancedTouch.Touch.onFingerDown += FingerDown;
+        EnhancedTouch.Touch.onFingerUp += FingerUp;
     }
 
     private void OnDisable()
@@ -20,13 +23,26 @@ public class Interact : MonoBehaviour
         EnhancedTouch.TouchSimulation.Disable();
         EnhancedTouch.EnhancedTouchSupport.Disable();
         EnhancedTouch.Touch.onFingerDown -= FingerDown;
+        EnhancedTouch.Touch.onFingerUp -= FingerUp;
     }
 
     private void FingerDown(EnhancedTouch.Finger finger)
     {
         if (finger.index != 0) return;
 
-        else
+        isFingerDown = true;
+    }
+
+    private void FingerUp(EnhancedTouch.Finger finger)
+    {
+        if (finger.index != 0) return;
+
+        isFingerDown = false;
+    }
+
+    private void Update()
+    {
+        if (isFingerDown)
         {
             float rotationAmt = rotationSpeed * Time.deltaTime;
             transform.Rotate(Vector3.right * rotationAmt);
